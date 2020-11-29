5c58458077a9f6f146e8b5502fb53dea55de6e3e
Lisbaldy <lisba7474@gmail.com>
2020-11-29 19:00:02 -0300

    tests agregados al TP3

---
+++ RecuperatoriosTP/TP3/Tests_Unitarios/Tests_Exceptions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Excepciones;
+using ClasesInstanciables;
+using EntidadesAbstractas;
+
+namespace Tests_Unitarios
+{
+    [TestClass]
+    public class Tests_Exceptions
+    {
+        [TestMethod]
+        [ExpectedException(typeof(AlumnoRepetidoException))]
+        public void TestAlumnoRepetidoExceptions()
+        {
+            Alumno alumnoUno = new Alumno(1, "Ramon", "Gonzalez", "25987547", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio, Alumno.EEstadoCuenta.AlDia);
+            Alumno alumnoDos = new Alumno(1, "Ramon", "Gonzalez", "25987547", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio, Alumno.EEstadoCuenta.AlDia);
+            Universidad universidad = new Universidad();
+            
+            universidad += alumnoUno;
+            universidad += alumnoDos;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DniInvalidoException))]
+        public void TestDniInvalidoException()
+        {
+            new Alumno(1, "Andres", "Castillo", "12234A58", Persona.ENacionalidad.Extranjero, Universidad.EClases.Laboratorio, Alumno.EEstadoCuenta.Deudor);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(NacionalidadInvalidaException))]
+        public void TestNacionalidadInvalidaException()
+        {
+            new Profesor(1, "Pepe", "Peposo", "95456273", Persona.ENacionalidad.Argentino);
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP3/Tests_Unitarios/Tests_Instancias.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClasesInstanciables;
+using EntidadesAbstractas;
+
+namespace Tests_Unitarios
+{
+    [TestClass]
+    public class Tests_Instancias
+    {
+        [TestMethod]
+        public void TestInstanciaAlumno()
+        {
+            bool resultado = false;
+            Universidad universidad = new Universidad();
+            Alumno alumno1 = new Alumno(1, "Andres", "Castillo", "12234458", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio, Alumno.EEstadoCuenta.Deudor);
+            Profesor profesor1 = new Profesor(1, "Profesor", "Jirafales", "95456273", Persona.ENacionalidad.Extranjero);
+
+            universidad += profesor1;
+            universidad += alumno1;
+
+            if (universidad.Alumnos.Count > 0 && universidad.Instructores.Count > 0 && universidad.Jornadas.Count == 0)
+            {
+                resultado = true;
+            }
+
+            Assert.IsTrue(resultado);
+        }
+
+        [TestMethod]
+        public void TestSobrecargaOperadorProfesorUniversidad()
+        {
+            bool resultado = false;
+            Profesor profesor = new Profesor();
+            Universidad universidad = new Universidad();
+
+            universidad += profesor;
+
+            if (universidad == profesor)
+            {
+                resultado = true;
+            }
+
+            Assert.IsTrue(resultado);
+        }
+
+        [TestMethod]
+        public void TestInstanciaProfesor()
+        {
+            bool resultado = false;
+            Profesor profesor1 = new Profesor(1, "Profesor", "Jirafales", "95456273", Persona.ENacionalidad.Extranjero);
+            Jornada jornada1 = new Jornada(Universidad.EClases.Laboratorio, profesor1);
+
+            if (jornada1.Instructor == profesor1)
+            {
+                resultado = true;
+            }
+
+            Assert.IsTrue(resultado);
+        }
+
+    }
+}
